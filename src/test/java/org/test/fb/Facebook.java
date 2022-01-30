7b2b4d88c57f37f26291b5350e25f288a22c3476
Kesavini <k7ekalai16@gmail.com>
2022-01-30 17:54:02 +0530

    vini code

--- src/test/java/org/test/fb/Facebook.java
+++ src/test/java/org/test/fb/Facebook.java
@@ -8,7 +8,8 @@ public class Facebook extends BaseClass{
 		
 		chromeLaunch();
 		urlLaunch("https://www.facebook.com/");
-	
+		System.out.println("test 5");
+	System.out.println("test 6");
 		
 		
 	}
