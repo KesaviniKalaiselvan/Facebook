94d16991af7e3605c46938c5ec3c7f9dd3a0d1bc
Kesavini <k7ekalai16@gmail.com>
2022-01-30 13:05:16 +0530

    kesav's update code

--- src/test/java/org/test/fb/Facebook.java
+++ src/test/java/org/test/fb/Facebook.java
@@ -1,6 +1,7 @@
 package org.test.fb;
 
 import org.test.baseclass.BaseClass;
+import org.test.pages.LoginPage;
 
 public class Facebook extends BaseClass{
 	
@@ -8,6 +9,12 @@ public class Facebook extends BaseClass{
 		
 		chromeLaunch();
 		urlLaunch("https://www.facebook.com/");
+		dynamicWait(30);
+		LoginPage l=new LoginPage();
+		
+		sendKeys(l.getTextusername(),"Kesavini" );
+		sendKeys(l.getTextpassword(), "12345678");
+		
 	
 		
 		
